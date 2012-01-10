8ef5c16030f412a7d312dd833e8e4c32f6211021
Michael JasonSmith <mpj17@onlinegroups.net>
2012-01-10 11:36:36 +1100

    Using the SitePage as the base of the GroupPage. This gives us the loggedInUserInfo for free.

--- gs/group/base/page.py
+++ gs/group/base/page.py
@@ -1,10 +1,15 @@
 # coding=utf-8
-from Products.Five import BrowserView
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
+from gs.content.base.page import SitePage
 
-class GroupPage(BrowserView):
+class GroupPage(SitePage):
     def __init__(self, group, request):
-        BrowserView.__init__(self, group, request)
-        self.siteInfo = createObject('groupserver.SiteInfo', group)
-        self.groupInfo = createObject('groupserver.GroupInfo', group)
+        SitePage.__init__(self, group, request)
+        self.group = group
+
+    @Lazy
+    def groupInfo(self):
+      retval = createObject('groupserver.GroupInfo', self.group)
+      return retval
 
