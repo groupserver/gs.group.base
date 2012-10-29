ba9167c9c5e5497aa62938157e7625b440123d7a
Michael JasonSmith <mpj17@onlinegroups.net>
2012-10-29 13:48:13 +1100

    Importing the major classes into the __init__

---
+++ gs/group/base/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#lint:disable
+from viewlet import GroupViewlet
+from page import GroupPage
+from form import GroupForm
+from contentprovider import GroupContentProvider
+#lint:enable
