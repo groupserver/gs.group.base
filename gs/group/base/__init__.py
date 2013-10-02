2da5733d55d8738348d13a8c3d26f1560fe417fb
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-02 11:13:38 -0700

    Using absolute_import, and adding a Copyright block.

--- gs/group/base/__init__.py
+++ gs/group/base/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #lint:disable
-from viewlet import GroupViewlet
-from page import GroupPage
-from form import GroupForm
-from contentprovider import GroupContentProvider
+from __future__ import absolute_import
+from .viewlet import GroupViewlet
+from .page import GroupPage
+from .form import GroupForm
+from .contentprovider import GroupContentProvider
 #lint:enable
