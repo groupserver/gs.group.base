292f02c0270c955d8e43d5288603290e5fba1c2b
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 12:41:47 -0700

    Following gs.content.form.base

--- gs/group/base/form.py
+++ gs/group/base/form.py
@@ -1,14 +1,30 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
-from gs.content.form.form import SiteForm
+from gs.content.form.base import SiteForm
 
 
 class GroupForm(SiteForm):
+    'The page view abstract base class for forms in a group'
     def __init__(self, context, request):
         super(GroupForm, self).__init__(context, request)
 
     @Lazy
     def groupInfo(self):
+        'Information about the current group.'
         retval = createObject('groupserver.GroupInfo', self.context)
         return retval
