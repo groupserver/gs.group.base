26b16b84bf1061d5bf6dcb02b8f8f4889fb180de
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 12:34:33 -0700

    Switching to Unicode literals

--- gs/group/base/interfaces.py
+++ gs/group/base/interfaces.py
@@ -1,20 +1,33 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
 from zope.interface import Interface
 from zope.schema import Field
 from Products.XWFChat.interfaces import IGSGroupFolder
 
 
 class IGSGroupMarker(IGSGroupFolder):
-    u'''The base marker-interface for groups.'''
-    pass
+    '''The base marker-interface for groups.'''
 
 
 # TODO: Create a field type for siteInfo and groupInfo instances.
 class IGSGroupPage(Interface):
-    siteInfo = Field(title=u'Site Information',
-        description=u'A SiteInfo instance.',
+    siteInfo = Field(title='Site Information',
+        description='A SiteInfo instance.',
         required=True)
 
-    groupInfo = Field(title=u'Group Information',
-        description=u'A GroupInfo instance.',
+    groupInfo = Field(title='Group Information',
+        description='GroupInfo instance.',
         required=True)
